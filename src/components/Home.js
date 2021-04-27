50fda213dc6d9bd8ec04d0e2c03ef692fa76ff3d
raisdhaka <raisdhaka@gmail.com>
2021-04-27 07:33:37 +0600

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,14 +41,7 @@ const Home = ()=>{
         })
     }
 
-    const images = [
-        "https://picsum.photos/200/400?image=206",
-        "https://picsum.photos/150/200?image=1050",
-        
-        "https://picsum.photos/300/500?image=106",
-        "https://picsum.photos/500/300?image=706",
-        "https://picsum.photos/300/200?image=1060",
-    ]
+
     
 
     let history = useHistory();
@@ -95,49 +88,5 @@ const Home = ()=>{
        
 }
 
-
-const CategoryList = (props) => {
-    let history = useHistory();
-
-    const renderItem = (data) => (
-        <ListGroup.Item key={data.id}>
-            <Media>
-                { data.thumb_image && <Image thumbnail src={data.thumb_image} className="mr-3" style={{ cursor: 'pointer' }} /> || <InsertPhotoIcon style={{ fontSize: 140 }}/> }
-                <Media.Body>
-                    <h2><span>{data.title}</span></h2>
-                    <p style={{ marginBottom: "10px" }}><span>{data.description}</span></p>
-                    { data.public_playlists && data.public_playlists.length > 0 && 
-                        <Button variant="primary" size="sm" style={{ padding: '5px 20px' }}
-                            className="mr-2"
-                            onClick={ () => { let playlist = data.playlists.filter(item => item.id == data.public_playlists[0])[0]; console.log(playlist); history.push('/playlist/' + playlist.playlist_id) } }>
-                            Open Public Playlists
-                        </Button>
-                    }
-                    { data.private_playlists && data.private_playlists.length > 0 &&
-                        <Button variant="primary" size="sm" style={{ padding: '5px 20px' }}
-                            className="mr-2"
-                            onClick={ () => { history.push('/category_playlists/' + data.category_id) } }>
-                            Open Private Playlists
-                        </Button>
-                    }
-                </Media.Body>
-            </Media>
-        </ListGroup.Item>
-    );
-
-
-    return (
-        <>
-            <div className="card">
-                <ListGroup variant="flush">
-                    {props.categories
-                        && props.categories.map(item => {
-                                return renderItem(item)
-                        })}
-                </ListGroup>
-            </div>
-        </>
-    );
-}
 
 export default Home;
