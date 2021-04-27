0f09c4c57b187c200a6a5c743e367e1f1a6ae698
raisdhaka <raisdhaka@gmail.com>
2021-04-27 07:32:27 +0600

    homePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,11 @@ import {
     Image,
     Button,
     ListGroup,
-    Media,
+    Media
 } from 'react-bootstrap';
+
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+
 
 const Home = ()=>{
     const [content, setContent] = useState();
@@ -38,15 +41,58 @@ const Home = ()=>{
         })
     }
 
-    return (
-        <div className="container">
-            {categories && categories.length && <CategoryList categories={categories}></CategoryList> || 
-                <div className="jumbotron">
-                    <h3>{content}</h3>
-                </div>
-            }
-        </div>
-    )
+    const images = [
+        "https://picsum.photos/200/400?image=206",
+        "https://picsum.photos/150/200?image=1050",
+        
+        "https://picsum.photos/300/500?image=106",
+        "https://picsum.photos/500/300?image=706",
+        "https://picsum.photos/300/200?image=1060",
+    ]
+    
+
+    let history = useHistory();
+            return (
+            <ResponsiveMasonry
+                columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+            >
+                <Masonry>
+
+                    {categories
+                        && categories.map((data, i) => {
+                                return(
+                                    <div className="m-1">
+                                    <Media className="masonryMedia">
+                                  <img  src={data.thumb_image} className="img100" style={{ cursor: 'pointer' }}                                  />  
+                                  <Media.Body class="media-body justify-content-center align-items-center">
+                                        <div>
+                                        <h2><span>{data.title}</span></h2>
+                                        <p style={{ marginBottom: "10px" }}><span>{data.description}</span></p>
+                                        { data.public_playlists && data.public_playlists.length > 0 && 
+                                            <Button variant="primary" size="sm" style={{ padding: '5px 20px' }}
+                                                className="mr-2"
+                                                onClick={ () => { let playlist = data.playlists.filter(item => item.id == data.public_playlists[0])[0]; console.log(playlist); history.push('/playlist/' + playlist.playlist_id) } }>
+                                                Open Public Playlists
+                                            </Button>
+                                        }
+                                        { data.private_playlists && data.private_playlists.length > 0 &&
+                                            <Button variant="primary" size="sm" style={{ padding: '5px 20px' }}
+                                                className="mr-2"
+                                                onClick={ () => { history.push('/category_playlists/' + data.category_id) } }>
+                                                Open Private Playlists
+                                            </Button>
+                                        }
+                                        </div>
+                                    </Media.Body>
+                                    </Media>  
+                                    </div>
+                                )   
+
+                        })}
+                </Masonry>
+            </ResponsiveMasonry>
+        )
+       
 }
 
 
